a9aa0ea74fe90dcb57add7eb4ae656fbb51c6512
bestarandyan <2238985517@qq.com>
2015-03-31 20:01:47 +0800

    update psw fragment  package

--- BestarProject/ShoppingMall/src/main/java/com/manyi/mall/user/UpdatePswFragment.java
+++ BestarProject/ShoppingMall/src/main/java/com/manyi/mall/user/UpdatePswFragment.java
@@ -1,13 +1,9 @@
 package com.manyi.mall.user;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.os.Handler;
-import android.os.Message;
 import android.text.TextUtils;
-import android.widget.Button;
 import android.widget.EditText;
 
 import com.huoqiu.framework.app.SuperFragment;
@@ -31,32 +27,32 @@ import org.androidannotations.annotations.ViewById;
 
 @EFragment(R.layout.fragment_update_password)
 public class UpdatePswFragment extends SuperFragment<Integer> {
-	@ViewById(R.id.oldPswEt)
-	EditText mOldPswEt;
-	@ViewById(R.id.newPswEt)
-	EditText mNewPswEt;
-	@ViewById(R.id.againPswEt)
+    @ViewById(R.id.oldPswEt)
+    EditText mOldPswEt;
+    @ViewById(R.id.newPswEt)
+    EditText mNewPswEt;
+    @ViewById(R.id.againPswEt)
     EditText mAgainPswEt;
 
-	@AfterViews
-	void init() {
+    @AfterViews
+    void init() {
 
-	}
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		ManyiUtils.closeKeyBoard(getActivity(), mAgainPswEt);
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ManyiUtils.closeKeyBoard(getActivity(), mAgainPswEt);
+    }
 
-	@UiThread
-	public void showDialog(final BaseResponse response) {
-		DialogBuilder.showSimpleDialog(response.getMessage(), getBackOpActivity(),new DialogInterface.OnClickListener() {
+    @UiThread
+    public void showDialog(final BaseResponse response) {
+        DialogBuilder.showSimpleDialog(response.getMessage(), getBackOpActivity(), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if (response.getCode() .equals("0") ){
+                if (response.getCode().equals("0")) {
                     String newPsw = mNewPswEt.getText().toString().trim();
-                    SharedPreferences userInfo= getActivity().getSharedPreferences("userInfo", Activity.MODE_PRIVATE);
+                    SharedPreferences userInfo = getActivity().getSharedPreferences("userInfo", Activity.MODE_PRIVATE);
                     SharedPreferences.Editor editor1 = userInfo.edit();
                     editor1.putString("password", newPsw);
                     editor1.commit();
@@ -66,58 +62,57 @@ public class UpdatePswFragment extends SuperFragment<Integer> {
                 }
             }
         });
-	}
+    }
 
 
-
-	@UiThread
-	@Click(R.id.getCodeNextBtn)
-	public void nextSuccess() {
-		if (CheckDoubleClick.isFastDoubleClick()) {
-			return;
-		}
-		if (TextUtils.isEmpty(mOldPswEt.getText().toString()) || TextUtils.isEmpty(mOldPswEt.getText().toString().trim())) {
-			DialogBuilder.showSimpleDialog("请输入原密码", getBackOpActivity());
-			return;
-		}else if (TextUtils.isEmpty(mNewPswEt.getText().toString()) || TextUtils.isEmpty(mNewPswEt.getText().toString().trim())) {
-			DialogBuilder.showSimpleDialog("请输入新密码", getBackOpActivity());
-			return;
-		}else if (TextUtils.isEmpty(mAgainPswEt.getText().toString()) || TextUtils.isEmpty(mAgainPswEt.getText().toString().trim())) {
+    @UiThread
+    @Click(R.id.getCodeNextBtn)
+    public void nextSuccess() {
+        if (CheckDoubleClick.isFastDoubleClick()) {
+            return;
+        }
+        if (TextUtils.isEmpty(mOldPswEt.getText().toString()) || TextUtils.isEmpty(mOldPswEt.getText().toString().trim())) {
+            DialogBuilder.showSimpleDialog("请输入原密码", getBackOpActivity());
+            return;
+        } else if (TextUtils.isEmpty(mNewPswEt.getText().toString()) || TextUtils.isEmpty(mNewPswEt.getText().toString().trim())) {
+            DialogBuilder.showSimpleDialog("请输入新密码", getBackOpActivity());
+            return;
+        } else if (TextUtils.isEmpty(mAgainPswEt.getText().toString()) || TextUtils.isEmpty(mAgainPswEt.getText().toString().trim())) {
             DialogBuilder.showSimpleDialog("请再次输入新密码", getBackOpActivity());
             return;
-        }else if(!mNewPswEt.getText().toString().trim().equals(mAgainPswEt.getText().toString().trim())){
+        } else if (!mNewPswEt.getText().toString().trim().equals(mAgainPswEt.getText().toString().trim())) {
             DialogBuilder.showSimpleDialog("两次输入的新密码不一致！", getBackOpActivity());
-        }else if(mOldPswEt.getText().toString().trim().equals(mNewPswEt.getText().toString().trim())){
+        } else if (mOldPswEt.getText().toString().trim().equals(mNewPswEt.getText().toString().trim())) {
             DialogBuilder.showSimpleDialog("新密码和原密码相同！", getBackOpActivity());
-        }else{
+        } else {
             updatePsw();
         }
-	}
+    }
 
     @Background
-    void updatePsw(){
+    void updatePsw() {
         String newPsw = mNewPswEt.getText().toString().trim();
         String oldPsw = mOldPswEt.getText().toString().trim();
         RequestServerFromHttp request = new RequestServerFromHttp();
-        String msg = request.updatePsw(newPsw,oldPsw);
+        String msg = request.updatePsw(newPsw, oldPsw);
         BaseResponse response = new JsonData().JsonBase(msg);
         showDialog(response);
     }
 
-	@Click(R.id.updatePswBack)
-	void back() {
-		if (CheckDoubleClick.isFastDoubleClick()) {
-			return;
-		}
-		ManyiUtils.closeKeyBoard(getActivity(), mAgainPswEt);
-		remove();
-	}
-	
-	@Click(R.id.getPswBtn)
-	void getPswClick() {
-		if (CheckDoubleClick.isFastDoubleClick()) {
-			return;
-		}
+    @Click(R.id.updatePswBack)
+    void back() {
+        if (CheckDoubleClick.isFastDoubleClick()) {
+            return;
+        }
+        ManyiUtils.closeKeyBoard(getActivity(), mAgainPswEt);
+        remove();
+    }
+
+    @Click(R.id.getPswBtn)
+    void getPswClick() {
+        if (CheckDoubleClick.isFastDoubleClick()) {
+            return;
+        }
         if (CheckDoubleClick.isFastDoubleClick())
             return;
         ForgetPasswordFragment fragment = GeneratedClassUtils.getInstance(ForgetPasswordFragment.class);
@@ -127,6 +122,6 @@ public class UpdatePswFragment extends SuperFragment<Integer> {
                 R.anim.anim_fragment_close_out);
         fragment.setManager(getFragmentManager());
         fragment.show(SHOW_ADD_HIDE);
-	}
+    }
 
 }
